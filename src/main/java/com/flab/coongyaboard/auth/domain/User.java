e2983e536d0fe5bbb8758c513addd53c7a14854c
coongya <moominie18@gmail.com>
2025-12-07 20:47:21 +0900

    feat: User 도메인, UserMapper 구현

---
+++ src/main/java/com/flab/coongyaboard/auth/domain/User.java
@@ -0,0 +1,21 @@
+package com.flab.coongyaboard.auth.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    private Long id;
+    private String email;
+    private String nickname;
+    private String password;
+    private LocalDateTime createdAt;
+}
