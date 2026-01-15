8efee43e22e0d8404fdfed6a1722a45e7ce485d8
coongya <moominie18@gmail.com>
2026-01-15 23:32:24 +0900

    fix: 에러코드 상수화하기

--- src/main/java/com/flab/coongyaboard/auth/exception/DuplicateEmailException.java
+++ src/main/java/com/flab/coongyaboard/auth/exception/DuplicateEmailException.java
@@ -4,7 +4,9 @@ import com.flab.coongyaboard.common.exception.BusinessException;
 
 public class DuplicateEmailException extends BusinessException {
 
+    private static final String DUPLICATE_EMAIL = "DUPLICATE_EMAIL";
+
     public DuplicateEmailException() {
-        super("email", "DUPLICATE_EMAIL", "이미 사용 중인 이메일입니다.");
+        super("email", DUPLICATE_EMAIL, "이미 사용 중인 이메일입니다.");
     }
 }
